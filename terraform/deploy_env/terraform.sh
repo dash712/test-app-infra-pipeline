63234aa7a59c1604200eb9b26f54bd44a4f31dc4
unknown <dashiell.lumas@gmail.com>
2021-06-08 15:21:14 -0400

    added conditional for palisade to terraform.sh

--- terraform/deploy_env/terraform.sh
+++ terraform/deploy_env/terraform.sh
@@ -4,8 +4,8 @@ ENVIRONMENT=$(echo "$2" | tr '[:upper]' '[:lower]')
 DEPLOYMENT_REGION=$(echo "$3" | tr '[:upper]' '[:lower]')
 APP_NAME=$(echo "$4" | tr '[:upper]' '[:lower]')
 MODULE=$(echo "$5" | tr '[:upper]' '[:lower]')
-VAR-FOLDER=$ENVIRONMENT-$DEPLOYMENT_REGION
-KEY="applications/$APP_NAME/$MODULE_TYPE/terraform.tfstate"
+# VAR-FOLDER=$ENVIRONMENT-$DEPLOYMENT_REGION
+# KEY="applications/$APP_NAME/$MODULE_TYPE/terraform.tfstate"
 
 cd terraform/$MODULE || exit 1
 /usr/local/bin/terraform init \
@@ -15,25 +15,34 @@ cd terraform/$MODULE || exit 1
 -backend-config="region=$REGION" \
 -get=true
 
-/usr/local/bin/terraform plan --out tfplan.binary
-/usr/local/bin/terraform show -json tfplan.binary > tfplan.json
+case $RUN_TYPE in
 
-case "$RUN_TYPE" in
-"plan" )
-        /usr/local/bin/terraform plan -var-file="$VAR_FOLDER/terraform.tfvars" -out=tfplan -input=false;;
-"apply" )
-        /usr/local/bin/terraform plan -var-file="$VAR_FOLDER/terraform.tfvars" -out=tfplan.binary -input=false;;
-        /usr/local/bin/terraform show -json tfplan.binary > tfplan.json 
-"destroy" )
-        /usr/local/bin/terraform destroy -force -var-file="$VAR_FOLDER/terraform.tfvars";;
-*   )
-    echo "Invalid action"; exit 1;;
+"plan")
+    /usr/local/bin/terraform plan -out=tfplan -input=false
+    ;;
+
+"apply")
+    /usr/local/bin/terraform plan -out=tfplan.binary -input=false
+    /usr/local/bin/terraform show -json tfplan.binary > tfplan.json
+    if [[ ($ENVIRONMENT == "uat" || $ENVIRONMENT == "prod") && $MODULE == "sg" ]]; then 
+        echo "Running palisade"
+        # /usr/local/bin/opa eval --format pretty -b . --input tfplan.json 
+        exit 0;
+        # Run palisade 
+        # palisade needs to be passed a tfplan.json, global policy, and application-specific policy in YAML/JSON format 
+
+        # if palisade returns policy violation; exit 1; else continue to terraform apply 
+    else
+        /usr/local/bin/terraform apply
+    fi 
+    ;;
+
+"destroy")
+    /usr/local/bin/terraform destroy -force
+    ;;
+
+*)
+    echo "Invalid action"
+    exit 1
+    ;;
 esac
-if [[ ($ENVIRONMENT == "uat" || $ENVIRONMENT == "prod") && $RUN_TYPE == "apply" && $MODULE == "sg" ]]; then 
-echo "Running palisade"
-exit 0;
-# Run palisade 
-# palisade needs to be passed a tfplan.json, global policy, and application-specific policy in YAML/JSON format 
-# opa eval --format pretty -b . --input tfplan.json 
-# if palisade returns policy violation; exit 1; else continue to terraform apply 
-fi
