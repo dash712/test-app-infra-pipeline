29fdbd2f22a11a2f21875d7173fb449c4349cdd9
unknown <dashiell.lumas@gmail.com>
2021-06-08 11:25:35 -0400

    set access

--- terraform/sg/main.tf
+++ terraform/sg/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    #  shared_credentials_file = "~/.aws/credentials"
-    profile = "default"
+     shared_credentials_file = "~/.aws/credentials"
+    # profile = "default"
 }
 
 variable "vpc_id" {
